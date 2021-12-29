d25860dc6a370d35c8f141ec78ecd62be1d50926
AlNahean <nahean.fardous@gmail.com>
2021-12-30 03:07:05 +0600

    make it better

--- baseUrl.js
+++ baseUrl.js
@@ -1,4 +1,5 @@
-const baseURL = "http://localhost:4000/";
+const baseURL = "https://daraz-clone-v1.herokuapp.com/";
+// const baseURL = "http://localhost:4000/";
 // const baseURL = "https://testing-v1-2021-08-31.herokuapp.com/";
 
 module.exports = baseURL;
